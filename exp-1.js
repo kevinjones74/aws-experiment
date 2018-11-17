8ed3a107de259cf8f2833246d16ded9cd318a6f5
Kevin Jones <Kevin@Kevins-iMac.home>
2018-11-17 15:42:45 +0000

    Initial commit

---
+++ exp-1.js
@@ -0,0 +1,63 @@
+// AWS Experiment - app to exercise S3
+
+var aws = require('aws-sdk'),
+    express = require('express'),
+    uuid = require('uuid');
+
+var s3 = new aws.S3();
+var app = express();
+
+// Status endpoint
+app.get('/status',function(req,resp){
+    resp.end('Running ok');
+    console.log('Status called');
+})
+
+// Create S3 bucket
+app.post('/s3CreateBucket/:bucket',function(req,resp){
+    console.log('s3Create called with ',req.params.bucket);
+    var bucket = req.params.bucket + uuid();
+    console.log('s3 bucket created - ',bucket);
+})
+
+// Store data in s3
+app.post('/s3Create/:id',function(req,resp){
+    console.log('s3Create called with ',req.params.id);
+    s3.putObject(
+        {Bucket: bucket,
+        Key: req.params.id,
+        Body: req.body,
+        Matadata:{test_label:'test_value'}
+        }
+    );
+    resp.end('Create s3 object: ' + request.params.id);
+})
+
+// Retrieve data from s3 bucket
+app.get('/s3Retrieve/:id',function(req,resp){
+    console.log('s3Retrieve called with ',req.params.id);
+    s3.getObject(
+      {Bucket: bucket,
+      Key: id},
+      function(err,data){
+          console.log(data);
+          resp.end('Retrieved data from Bucket: ' + bucket +'\n' + 'Key: ' + id +'\n' + 'Data: ' +data);
+      }  
+    )
+})
+
+// Delete data from s3
+app.delete('s3Delete/:id',function(req,resp){
+    console.log('s3Delete called with ',req.params.id);
+    s3.deletetObject(
+      {Bucket: bucket,
+      Key: id},
+      function(err,data){
+          console.log(data);
+          resp.end('Deleted data from Bucket: ' + bucket +'\n' + 'Key: ' + id +'\n' + 'Data: ' +data);
+      }  
+    ) 
+})
+
+// Start web server
+app.listen(8080);
